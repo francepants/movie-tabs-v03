7064dae1fbe04df87d3c223204abb37e2229d156
francepants <franceline90@gmail.com>
2020-08-20 01:13:53 -0500

    links so that current user can edit or delete their own reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
 
-    helper_method :current_user, :logged_in? #helper gives access to methods in the views
+    helper_method :current_user, :logged_in?, :current_user? #helper gives access to methods in the views
 
     private
     def current_user
         # if @current user is already defined then it will = @current user or it will find by id in users table
         @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+    def current_user?(user)
+        user == current_user
     end
 
     def logged_in?
