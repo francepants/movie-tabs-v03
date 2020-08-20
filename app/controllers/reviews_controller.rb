4ba6c1b8546c1c261c40cfdd61ffdc04ada392d5
francepants <franceline90@gmail.com>
2020-08-20 00:59:00 -0500

    can add reviews and see list of reviews, updated notes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,39 @@
 class ReviewsController < ApplicationController
+    before_action :current_user, only: [:edit, :update, :destroy]
+
+    def index
+        # if its nested AND if we can find that post 
+        if params[:movie_id] && @movie = Movie.find_by_id(params[:movie_id])
+            @reviews = @movie.reviews
+        else
+            @reviews = Review.all
+        end
+    end
+
+    def new
+        if params[:movie_id] && @movie = Movie.find_by_id[:movie_id]
+            @review = @movie.reviews.build
+        else
+            @review = Review.new
+        end
+    end
+
+    def create
+        @review = current_user.reviews.build(review_params)
+        @review.user = current_user
+        if @review.save
+            redirect_to reviews_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def set_review
+        @review = Review.find_by_id(params[:id])
+    end
+
+    def review_params
+        params.require(:review).permit(:title, :comment, :recommend, :movie_id, :user_id)
+    end
 end
